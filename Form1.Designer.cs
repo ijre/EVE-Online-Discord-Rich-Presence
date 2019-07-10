41ae679a8135e0ebaec4942c1ac604726c39a4c2
Paige <33820042+ijre@users.noreply.github.com>
2019-07-10 09:03:06 -0400

    working base

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace eve_discord_rpc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.englishCB = new System.Windows.Forms.CheckBox();
+            this.ingameCB = new System.Windows.Forms.CheckBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 26);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(65, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(163, 45);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Use the button below to select your most recent Gamelogs file.";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.checkBox1.Location = new System.Drawing.Point(175, 101);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 2;
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(97, 98);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(95, 59);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Is your game in any language that isn\'t English?";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(97, 163);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(95, 79);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "Would you like your presence to be in English, or your in game language?";
+            // 
+            // englishCB
+            // 
+            this.englishCB.AutoSize = true;
+            this.englishCB.Location = new System.Drawing.Point(65, 248);
+            this.englishCB.Name = "englishCB";
+            this.englishCB.Size = new System.Drawing.Size(60, 17);
+            this.englishCB.TabIndex = 7;
+            this.englishCB.Text = "English";
+            this.englishCB.UseVisualStyleBackColor = true;
+            this.englishCB.CheckedChanged += new System.EventHandler(this.EnglishCB_CheckedChanged);
+            // 
+            // ingameCB
+            // 
+            this.ingameCB.AutoSize = true;
+            this.ingameCB.Location = new System.Drawing.Point(175, 248);
+            this.ingameCB.Name = "ingameCB";
+            this.ingameCB.Size = new System.Drawing.Size(64, 17);
+            this.ingameCB.TabIndex = 8;
+            this.ingameCB.Text = "In-game";
+            this.ingameCB.UseVisualStyleBackColor = true;
+            this.ingameCB.CheckedChanged += new System.EventHandler(this.IngameCB_CheckedChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(260, -1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(41, 45);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(198, 63);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(70, 75);
+            this.textBox4.TabIndex = 10;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 63);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 75);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Undock";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(300, 285);
+            this.ControlBox = false;
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ingameCB);
+            this.Controls.Add(this.englishCB);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "EVE Online Discord RPC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.CheckBox englishCB;
+        private System.Windows.Forms.CheckBox ingameCB;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using DiscordRPC;
+
+namespace eve_discord_rpc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Thread.Sleep(15);
+            try
+            {
+                if (File.ReadAllText(Application.StartupPath + "/settings/" + "checkbox setting 1.txt").ToString() == "true")
+                    checkBox1.Checked = true;
+                else checkBox1.Checked = false;
+            }
+            catch { FolderCheckem(); }
+            try
+            {
+                if (File.ReadAllText(Application.StartupPath + "/settings/" + "checkbox setting 2.txt").ToString() == "ingame")
+                {
+                    ingameCB.Checked = true;
+                    englishCB.Checked = false;
+                }
+                else if (File.ReadAllText(Application.StartupPath + "/settings/" + "checkbox setting 2.txt").ToString() == "english")
+                {
+                    ingameCB.Checked = false;
+                    englishCB.Checked = true;
+                }
+            }
+            catch { FolderCheckem(); }
+        }
+
+        private void PresenceUpdate(string charName, string state)
+        {
+            DiscordRpcClient client = new DiscordRpcClient("598364146727124993");
+            client.SetPresence(new RichPresence { Details = "Playing EVE as: " + charName, State = state });
+            client.Initialize();
+            //textBox4.Text = "Presence updated!";
+            Thread.Sleep(5000);
+            Application.ExitThread();
+        }
+
+        private void Loop(string fileDir)
+        {
+            //textBox4.Text = "Parsing file...";
+            Process process = Process.GetProcessesByName("exefile")[0];
+
+            var data = File.ReadAllText(fileDir).ToString();
+            var charName = process.MainWindowTitle.Substring(6);
+
+            if (checkBox1.Checked)
+                if (englishCB.Checked) { }
+                //TODO: game is fucked and i cant switch it off of french, so i need to find a gamelog in english
+                else if (ingameCB.Checked)
+                {
+                    var location1 = data.LastIndexOf("<");
+                    var location2 = data.LastIndexOf(">");
+                    if (data.Substring(data.IndexOf("]", location2) + 42, 8) == "acceptée")
+                    {
+                        var half = data.Substring(location1, location2 - location1);
+                        var full = half.Substring(half.IndexOf("\"") + 1, (half.LastIndexOf("\"") - half.IndexOf("\"")) - 1);
+                        Thread presenceThread = new Thread(() => PresenceUpdate(charName, full));
+                        presenceThread.Start();
+                        presenceThread.Join();
+                        Loop(fileDir);
+                    }
+                }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog diag = new OpenFileDialog
+            {
+                Title = "Please select your most recent Gamelogs file.",
+                InitialDirectory = Application.StartupPath,
+                Multiselect = false,
+                RestoreDirectory = true,
+                DefaultExt = ".txt",
+                Filter = "Text files only|*.txt",
+                AddExtension = true,
+            };
+            diag.ShowDialog();
+            Thread loopThread = new Thread(() => Loop(diag.FileName));
+            loopThread.Start();
+        }
+
+        private void FolderCheckem()
+        {
+            if (!File.Exists(Application.StartupPath + "/settings/"))
+                Directory.CreateDirectory(Application.StartupPath + "/settings/");
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            FolderCheckem();
+            if (checkBox1.Checked == true)
+            {
+                File.WriteAllText(Application.StartupPath + "/settings/" + "checkbox setting 1.txt", "true");
+                englishCB.Visible = true;
+                ingameCB.Visible = true;
+                textBox3.Visible = true;
+            }
+            else
+            {
+                File.WriteAllText(Application.StartupPath + "/settings/" + "checkbox setting 1.txt", "false");
+                englishCB.Visible = false;
+                ingameCB.Visible = false;
+                textBox3.Visible = false;
+            }
+        }
+
+        private void EnglishCB_CheckedChanged(object sender, EventArgs e)
+        {
+            FolderCheckem();
+            if (englishCB.Checked == true)
+            {
+                if (ingameCB.Checked == true)
+                    ingameCB.Checked = false;
+
+                File.WriteAllText(Application.StartupPath + "/settings/" + "checkbox setting 2.txt", "english");
+            }
+        }
+
+        private void IngameCB_CheckedChanged(object sender, EventArgs e)
+        {
+            FolderCheckem();
+            if (ingameCB.Checked == true)
+            {
+                if (englishCB.Checked == true)
+                    englishCB.Checked = false;
+
+                File.WriteAllText(Application.StartupPath + "/settings/" + "checkbox setting 2.txt", "ingame");
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            DiscordRpcClient client = new DiscordRpcClient("598364146727124993");
+            client.SetPresence(new RichPresence { Details = "", State = "" });
+            Thread.Sleep(50);
+            client.Dispose();
+            Process process = Process.GetProcessesByName("eve-discord-rpc")[0];
+            process.Kill();
+        }
+    }
+}
