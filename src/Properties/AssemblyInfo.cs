5667e87699f798513e2fb9dbcc800fa5b74c9d3d
Paige <33820042+ijre@users.noreply.github.com>
2020-03-15 15:42:23 -0400

    Updated license year, modified description and added GitHub name

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
 [assembly: AssemblyTitle("eve-discord-rpc")]
-[assembly: AssemblyDescription("Use Discord-RPC while playing EVE Online!")]
+[assembly: AssemblyDescription("Connect your Discord RPC to EVE Online!")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("@ijre")]
 [assembly: AssemblyProduct("eve-discord-rpc")]
-[assembly: AssemblyCopyright("Copyright © Paige Seivertson 2019")]
+[assembly: AssemblyCopyright("Copyright © Paige Seivertson 2019-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
